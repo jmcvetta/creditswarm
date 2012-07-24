8ea59e8c1ce41ab2458e90ec7fcc425da596030a
Jason McVetta <jason.mcvetta@gmail.com>
2012-07-24 13:10:32 -0700

    configure apps

--- creditdispute/settings.py
+++ creditdispute/settings.py
@@ -1,5 +1,8 @@
 # Django settings for creditdispute project.
 
+import dj_database_url
+import os
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -9,16 +12,9 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
-}
+DATABASES = {'default': dj_database_url.config(default='sqlite:///' +
+        os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'db.sqlite') )}
+
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -27,7 +23,7 @@ DATABASES = {
 # timezone as the operating system.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = 'America/Los_Angeles'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
@@ -122,6 +118,9 @@ INSTALLED_APPS = (
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    'south',
+    'registration',
+    'gunicorn',
 )
 
 # A sample logging configuration. The only tangible logging
@@ -152,3 +151,10 @@ LOGGING = {
         },
     }
 }
+
+
+#
+# django-registration
+#
+ACCOUNT_ACTIVATION_DAYS = 7
+REGISTRATION_OPEN = True
