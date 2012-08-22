093d3984b7dd688fad13178d63796dbe8994b712
Jason McVetta <jason.mcvetta@gmail.com>
2012-08-21 17:16:11 -0700

    work on creditdispute models

--- creditdispute/admin.py
+++ creditdispute/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from creditdispute.models import Dispute
 
 class DisputeAdmin(admin.ModelAdmin):
-    list_display = ['pk', 'user', 'ts_updated', 'bureau', 'report_number']
+    list_display = ['pk', 'user', 'ts_updated']
 
 admin.site.register(Dispute, DisputeAdmin)
--- creditdispute/models.py
+++ creditdispute/models.py
@@ -7,7 +7,8 @@ STATUS_CHOICES = [
     ('S', 'Sent'),
     ]
 
-BUREAU_CHOICES = [
+# Credit Reporting Agencies
+CRA_CHOICES = [
     ('xp', 'Experian'),
     ('eq', 'Equifax'),
     ('tu', 'TransUnion'),
@@ -34,46 +35,57 @@ WRONG_INFO_TYPE_CHOICES = [
     ('phone', 'Telephone'),
     ]
 
+class CreditReport(models.Model):
+    '''
+    A credit report, furnished by a Credit Reporting Agency
+    '''
+    user = models.ForeignKey(User, blank=False)
+    cra = models.CharField(max_length=128, blank=False, choices=CRA_CHOICES, 
+        verbose_name='Credit Reporting Agency')
+    report_number = models.CharField(max_length=128, blank=False)
+    
+    class Meta:
+        unique_together = ['user', 'cra', 'report_number']
+    
+    def __str__(self):
+        return '%s:%s' % (self.get_cra_display().lower(), self.report_number)
+
+
+class Inquiry(models.Model):
+    '''
+    What is this?  Is it tied to a user in general, or only to a specific 
+    credit report, or...?
+    '''
+    inquiry_company_name = models.CharField(max_length=128, blank=True, null=True)
+    inquiry_date = models.DateField(blank=True, null=True)
+
+
+class Detail(models.Model):
+    '''
+    A disputed item on a credit report
+    '''
+    report = models.ForeignKey(CreditReport, blank=False)
+    #
+    # Disputed Account
+    # - This should be split into a separate model with a M2M relationship
+    #
+    company_name = models.CharField(max_length=128, blank=True, null=True)
+    account_number = models.CharField(max_length=128, blank=True, null=True)
+    reason = models.CharField(max_length=32, blank=True, null=True, 
+        choices=ACCT_DISPUTE_REASON_CHOICES)
+    other_reason = models.TextField(blank=True, null=True)
+
 
 class Dispute(models.Model):
+    '''
+    A credit report dispute
+    '''
     user = models.ForeignKey(User, blank=False)
     ts_created = models.DateTimeField(auto_now_add=True, blank=False, 
         verbose_name='Created Timestamp')
     ts_updated = models.DateTimeField(auto_now=True, blank=False, 
         verbose_name='Updated Timestamp')
     status = models.CharField(max_length=3, blank=False, choices=STATUS_CHOICES)
-    #
-    # Personal Info 
-    # - use encrypted fields before running in production!
-    #
-    given_name = models.CharField(max_length=128, blank=False)
-    family_name = models.CharField(max_length=128, blank=False)
-    address1 = models.CharField(max_length=128, blank=False)
-    address2 = models.CharField(max_length=128, blank=True, null=True)
-    city = models.CharField(max_length=128, blank=False)
-    state = models.CharField(max_length=128, blank=False)
-    zip = models.CharField(max_length=128, blank=False)
-    date_of_birth = models.DateField(blank=False)
-    ssn = models.CharField(max_length=128, blank=False)
-    #
-    # Credit Report(s)
-    #
-    report_number = models.CharField(max_length=128, blank=False)
-    bureau = models.CharField(max_length=128, blank=False, choices=BUREAU_CHOICES)
-    #
-    # Disputed Account
-    # - This should be split into a separate model with a M2M relationship
-    #
-    dispute_company_name = models.CharField(max_length=128, blank=True, null=True)
-    dispute_account_number = models.CharField(max_length=128, blank=True, null=True)
-    dispute_reason = models.CharField(max_length=32, blank=True, null=True, 
-        choices=ACCT_DISPUTE_REASON_CHOICES)
-    dispute_other_reason = models.TextField(blank=True, null=True)
-    #
-    # Inquiries
-    #
-    inquiry_company_name = models.CharField(max_length=128, blank=True, null=True)
-    inquiry_date = models.DateField(blank=True, null=True)
     #
     # Wrong Info
     #
