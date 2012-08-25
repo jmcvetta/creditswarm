2d73e1296a60f0f2b8d67778643b3dfc2c57d17a
Jason McVetta <jason.mcvetta@gmail.com>
2012-08-24 22:56:08 -0700

    create UserProfileView

--- accounts/views.py
+++ accounts/views.py
@@ -1,1 +1,51 @@
-# Create your views here.
+from django.forms import ModelForm
+from django.views.generic.edit import FormView
+from django.views.generic.edit import UpdateView
+from django.views.generic.edit import CreateView
+from django.contrib.localflavor.us.forms import USSocialSecurityNumberField
+from django.contrib.localflavor.us.forms import USStateField
+from django.contrib.localflavor.us.forms import USStateSelect
+from django.contrib.localflavor.us.forms import USZipCodeField
+
+from accounts.models import UserProfile
+
+
+class UserProfileForm(ModelForm):
+    class Meta:
+        model = UserProfile
+        exclude = ['user']
+    
+    ssn = USSocialSecurityNumberField(required=True)
+    state = USStateField(required=True, widget=USStateSelect)
+    zip = USZipCodeField(required=False)
+
+class UserProfileView(UpdateView):
+    '''
+    Edit profile for current user.
+    '''
+    model = UserProfile
+    form_class = UserProfileForm
+    success_url = '/'
+    
+    def get_initial(self):
+        '''
+        If user does not already have a profile, then populate form intial 
+        values with given and family names from User object.
+        '''
+        initial = self.initial
+        if not UserProfile.objects.filter(user=self.request.user):
+            initial['given_name'] = self.request.user.first_name
+            initial['family_name'] = self.request.user.last_name
+        return initial
+
+    
+    def get_object(self, queryset=None):
+        try:
+            return UserProfile.objects.get(user=self.request.user)
+        except UserProfile.DoesNotExist:
+            return None
+
+    
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(UserProfileView, self).form_valid(form)
--- pijyn/urls.py
+++ pijyn/urls.py
@@ -3,8 +3,12 @@
 
 from django.conf.urls import patterns, include, url
 from django.core.urlresolvers import reverse
+from django.views.generic.edit import CreateView
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib import admin
+from accounts.models import UserProfile
+from accounts.views import UserProfileView
+from accounts.models import profile_required
 from dispute.views import LoginView
 from dispute.views import home_view
 from dispute.views import dispute_wizard_view
@@ -17,7 +21,7 @@ urlpatterns = patterns('',
     # url(r'^pijyn/', include('pijyn.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
@@ -29,11 +33,18 @@ urlpatterns = patterns('',
         { 'next_page': '/' },
         name='logout',
         ),
+    #
+    # Account management
+    #
 	url(r'^accounts/', include('registration.backends.default.urls')),
+    url(r'^accounts/profile/edit$', 
+        UserProfileView.as_view(),
+        name='edit_profile',
+        ),
     #
     # 'dispute' module
     #
-    url(r'^dispute/new/$', dispute_wizard_view, name='new_dispute'),
+    url(r'^dispute/new/$', profile_required(dispute_wizard_view), name='new_dispute'),
 
 )
 
