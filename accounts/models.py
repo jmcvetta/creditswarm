9b5cdf830ff40cb0e3d5c2dacef0bafd00a8a169
Jason McVetta <jason.mcvetta@gmail.com>
2012-08-24 21:26:32 -0700

    Add @profile_required decorator

--- accounts/models.py
+++ accounts/models.py
@@ -1,11 +1,12 @@
 # Copyright (c) 2012 Jason McVetta.  This is Free Software, released under the
 # terms of the AGPL v3.  See www.gnu.org/licenses/agpl-3.0.html for details.
 
+from functools import wraps
+
 from django.db import models
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
 from django.contrib.auth.models import User
-from django.db.models.signals import post_save
-from django.contrib.localflavor.us.us_states import US_STATES
-from django.contrib.localflavor.us.models import USPostalCodeField
 
 
 class UserProfile(models.Model):  
@@ -16,10 +17,24 @@ class UserProfile(models.Model):
     #
     given_name = models.CharField(max_length=128)
     family_name = models.CharField(max_length=128)
-    address1 = models.CharField(max_length=128)
-    address2 = models.CharField(max_length=128)
+    address1 = models.CharField(max_length=128, verbose_name='Address 1')
+    address2 = models.CharField(max_length=128, null=True, blank=True, verbose_name="Address 2")
     city = models.CharField(max_length=128)
     state = models.CharField(max_length=128)
     zip = models.CharField(max_length=128)
     date_of_birth = models.DateField()
     ssn = models.CharField(max_length=128)
+
+def profile_required(method):
+    '''
+    Decorator that redirects user to profile creation page if they 
+    do not have a user profile.
+    '''
+    @wraps(method)
+    def wrapper(request, *args, **kwargs):
+        try:
+            request.user.get_profile()
+        except UserProfile.DoesNotExist:
+            return HttpResponseRedirect(reverse('new_profile'))
+        return method(request, *args, **kwargs)
+    return wrapper
