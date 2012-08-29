5f2704dd80c5462aa298168b3de6dd4d30ad2f10
Jason McVetta <jason.mcvetta@gmail.com>
2012-08-28 19:02:55 -0700

    Add admin views for Account and Inquiry

--- dispute/admin.py
+++ dispute/admin.py
@@ -3,8 +3,18 @@
 
 from django.contrib import admin
 from dispute.models import Dispute
+from dispute.models import Account
+from dispute.models import Inquiry
 
 class DisputeAdmin(admin.ModelAdmin):
     list_display = ['pk', 'user', 'ts_updated']
 
+class AccountAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'dispute', 'creditor', 'account_number']
+
+class InquiryAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'dispute', 'company_name', 'date']
+
 admin.site.register(Dispute, DisputeAdmin)
+admin.site.register(Account, AccountAdmin)
+admin.site.register(Inquiry, InquiryAdmin)
