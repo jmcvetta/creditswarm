79444e0163e664c55c073205fcdf04958cbf5f5f
Jason McVetta <jason.mcvetta@gmail.com>
2012-09-20 19:20:12 -0700

    Fully (?) working email send apparatus.

--- case/tasks.py
+++ case/tasks.py
@@ -1,5 +1,7 @@
 # Copyright (c) 2012 Jason McVetta.
 
+from django.contrib.sites.models import Site
+#
 from celery.task import Task
 from celery.registry import tasks
 #
@@ -17,20 +19,26 @@ class SendDisputeEmailTask(Task):
     '''
     
     def run(self, case_obj, **kwargs):
+        '''
+        Sends a dispute email for a given Case object.
+        '''
         #c = Case.objects.get(pk=case_pk)
         send_templated_mail(
-        template_name = 'cra_dispute_notification',
-        from_email = DEFAULT_FROM_EMAIL,
-        recipient_list = [AGENCIES[case_obj.agency]['email'],],
-        context = {
-        'object': case_obj,
-        },
-        # Optional:
-        # cc=['cc@example.com'],
-        # bcc=['bcc@example.com'],
-        # headers={'My-Custom-Header':'Custom Value'},
-        # template_ 
-        template_suffix = 'html',
-        )
+            template_name = 'cra_dispute_notification',
+            from_email = DEFAULT_FROM_EMAIL,
+            recipient_list = [AGENCIES[case_obj.agency]['email'],],
+            context = {
+                'object': case_obj,
+                'current_site': Site.objects.get_current(),
+                },
+            # Optional:
+            # cc=['cc@example.com'],
+            # bcc=['bcc@example.com'],
+            # headers={'My-Custom-Header':'Custom Value'},
+            # template_ 
+            template_suffix = 'html',
+            )
+        case_obj.status = 'S'
+        case_obj.save()
         
 tasks.register(SendDisputeEmailTask)
